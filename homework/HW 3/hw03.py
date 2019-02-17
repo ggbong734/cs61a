05baab8a7c47db5ecae5c257a11c4c3ad92f70e5
ggbong734 <ggbong734@gmail.com>
2019-02-17 14:57:19 -0800

    Add files via upload

---
+++ homework/HW 3/hw03.py
@@ -0,0 +1,155 @@
+HW_SOURCE_FILE = 'hw03.py'
+
+#############
+# Questions #
+#############
+
+from operator import add, mul, sub
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+def has_seven(k):
+    """Returns True if at least one of the digits of k is a 7, False otherwise.
+
+    >>> has_seven(3)
+    False
+    >>> has_seven(7)
+    True
+    >>> has_seven(2734)
+    True
+    >>> has_seven(2634)
+    False
+    >>> has_seven(734)
+    True
+    >>> has_seven(7777)
+    True
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'has_seven',
+    ...       ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if k < 10: 
+        if k == 7:
+            return True
+        else:
+            return False
+    if k % 10 == 7:
+        return True
+    else:
+        return has_seven(k // 10)
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def inc_or_dec(down_up, value):
+        if down_up == 1:
+            return value + 1
+        if down_up == 0:
+            return value - 1
+    
+    def helper(down_up, value, position):     
+        if position == n:
+            #print("down_up: ", down_up," , value: ", value, " , position: ", position)
+            return value
+        
+        elif (position % 7 == 0) or (has_seven(position)):
+            #print("down_up: ", down_up," , value: ", value, " , position: ", position)
+            return helper(1 - down_up, inc_or_dec(1-down_up, value), position+1)
+        
+        else:
+            #print("down_up: ", down_up," , value: ", value, " , position: ", position)
+            return helper(down_up, inc_or_dec(down_up, value), position+1)
+        
+    return helper(1, 1, 1)
+
+def accumulate(combiner, base, n, term):
+    """Return the result of combining the first n terms in a sequence and base.
+    The terms to be combined are term(1), term(2), ..., term(n).  combiner is a
+    two-argument, associative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
+    72
+    """
+    total, k = base, 1
+    while k <= n:
+        total, k = combiner(total, term(k)), k + 1
+    return total
+
+def filtered_accumulate(combiner, base, pred, n, term):
+    """Return the result of combining the terms in a sequence of N terms
+    that satisfy the predicate pred. combiner is a two-argument function.
+    If v1, v2, ..., vk are the values in term(1), term(2), ..., term(N)
+    that satisfy pred, then the result is
+         base combiner v1 combiner v2 ... combiner vk
+    (treating combiner as if it were a binary operator, like +). The
+    implementation uses accumulate.
+
+    >>> filtered_accumulate(add, 0, lambda x: True, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> filtered_accumulate(add, 11, lambda x: False, 5, identity) # 11
+    11
+    >>> filtered_accumulate(add, 0, odd, 5, identity)   # 0 + 1 + 3 + 5
+    9
+    >>> filtered_accumulate(mul, 1, greater_than_5, 5, square)  # 1 * 9 * 16 * 25
+    3600
+    >>> # Do not use while/for loops or recursion
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'filtered_accumulate',
+    ...       ['While', 'For', 'Recursion'])
+    True
+    """
+    def combine_if(x, y):
+        if pred(y):
+            return combiner(x,y)
+        return x
+    return accumulate(combine_if, base, n, term)
+
+def odd(x):
+    return x % 2 == 1
+
+def greater_than_5(x):
+    return x > 5
---
+++ homework/HW 4/hw04.py
@@ -0,0 +1,192 @@
+HW_SOURCE_FILE = 'hw04.py'
+import math
+###############
+#  Questions  #
+###############
+
+def intersection(st, ave):
+    """Represent an intersection using the Cantor pairing function."""
+    return (st+ave)*(st+ave+1)//2 + ave
+
+def street(inter):
+    return w(inter) - avenue(inter)
+
+def avenue(inter):
+    return inter - (w(inter) ** 2 + w(inter)) // 2
+
+w = lambda z: int(((8*z+1)**0.5-1)/2)
+
+def taxicab(a, b):
+    """Return the taxicab distance between two intersections.
+
+    >>> times_square = intersection(46, 7)
+    >>> ess_a_bagel = intersection(51, 3)
+    >>> taxicab(times_square, ess_a_bagel)
+    9
+    >>> taxicab(ess_a_bagel, times_square)
+    9
+    """
+    "*** YOUR CODE HERE ***"
+    return abs(street(a)-street(b)) + abs(avenue(a)-avenue(b))
+
+def squares(s):
+    """Returns a new list containing square roots of the elements of the
+    original list that are perfect squares.
+
+    >>> seq = [8, 49, 8, 9, 2, 1, 100, 102]
+    >>> squares(seq)
+    [7, 3, 1, 10]
+    >>> seq = [500, 30]
+    >>> squares(seq)
+    []
+    """
+    "*** YOUR CODE HERE ***"
+    new_list =[]
+    i = 0
+    while i < len(s):
+        if s[i]**0.5 == round(s[i]**0.5):
+            new_list.append(round(s[i]**0.5))
+        i += 1
+    return new_list    
+    
+def g(n):
+    """Return the value of G(n), computed recursively.
+
+    >>> g(1)
+    1
+    >>> g(2)
+    2
+    >>> g(3)
+    3
+    >>> g(4)
+    10
+    >>> g(5)
+    22
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'g', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n <=3:
+        return n
+    elif n > 3:
+        return g(n-1) + 2*g(n-2) + 3*g(n-3)
+
+def g_iter(n):
+    """Return the value of G(n), computed iteratively.
+
+    >>> g_iter(1)
+    1
+    >>> g_iter(2)
+    2
+    >>> g_iter(3)
+    3
+    >>> g_iter(4)
+    10
+    >>> g_iter(5)
+    22
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'g_iter', ['Recursion'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    
+    if n <= 3:
+        return n
+    elif n > 3:
+        i = 3
+        prev_2, prev, curr, next1  = 1, 2, 3, 0
+        while i < n:
+            next1 = curr + 2*prev + 3*prev_2
+            curr, prev, prev_2 = next1, curr, prev
+            i += 1       
+        return curr
+
+def count_change(amount):
+    """Return the number of ways to make change for amount.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'count_change', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    
+    def helper(x, i):
+        if x == 0:
+            return 1
+        elif x < 0:
+            return 0
+        else:
+            def iterator(n):
+                if n > i:
+                    return 0
+                else:
+                    return helper(x-2**n, n) + iterator(n+1) 
+            return iterator(0)
+            '''
+            k, total = 0, 0
+            while k <= i:
+                total += helper(x-2**k, k)
+                k += 1
+            return total                          
+            '''
+    return helper(amount, math.floor(math.log(amount,2)))
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    "*** YOUR CODE HERE ***"
+
+###################
+# Extra Questions #
+###################
+
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return lambda n:(lambda x,n : x(x,n))(lambda x,n: 1 if n == 1 else  n*x(x,n-1), n)
