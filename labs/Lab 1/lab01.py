88f42b687c84ca71d48fa783b37f005a44d40189
ggbong734 <ggbong734@gmail.com>
2019-02-17 14:53:40 -0800

    Add files via upload

---
+++ labs/Lab 1/lab01.py
@@ -0,0 +1,34 @@
+"""Lab 1: Expressions and Control Structures"""
+
+# Q3
+def both_positive(x, y):
+    """Returns True if both x and y are positive.
+
+    >>> both_positive(-1, 1)
+    False
+    >>> both_positive(1, 1)
+    True
+    """
+    return x > 0 and y > 0 # You can replace this line!
+
+# Q4
+def sum_digits(n):
+    """Sum all the digits of n.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    """
+    quotient = n
+    sum_digits = 0
+    
+    while quotient > 0: 
+        remainder = quotient % 10
+        quotient = quotient //10
+        sum_digits += remainder
+        
+    return sum_digits 
+    "*** YOUR CODE HERE ***"
